d8bc31f90321333e71105c26bce426bb28f1fdaf
coolguy1753 <chann.singh90@gmail.com>
2025-08-21 01:14:37 -0500

    Linkedin Icon

--- million-square-solutions 2/src/components/AboutSection.jsx
+++ million-square-solutions 2/src/components/AboutSection.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { Award, Target, Users, Zap } from 'lucide-react';
+import { Award, Target, Users, Zap, Linkedin } from 'lucide-react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
